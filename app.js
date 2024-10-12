5f9e259ca7a097704ce364364840db676f65c3c1
dillescas01 <165909998+dillescas01@users.noreply.github.com>
2024-10-12 04:19:14 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.get('/', (req, res) => {
 // Usar las rutas de usuarios con prefijo /users
 app.use('/users', usersRoutes);
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+
 // Conectar a la base de datos MongoDB
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/usuarios';
 mongoose.connect(mongoUri, {
